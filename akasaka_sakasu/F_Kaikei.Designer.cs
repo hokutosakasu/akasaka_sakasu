7adabbf1bc7214517d42a65f43e24fb4a1b25827
hokuto akasaka <hokutosakasu@gmail.com>
2021-02-01 22:41:46 +0900

    210201

--- akasaka_sakasu/F_Kaikei.Designer.cs
+++ akasaka_sakasu/F_Kaikei.Designer.cs
@@ -30,27 +30,184 @@
         {
             this.Txt_1 = new System.Windows.Forms.TextBox();
             this.Lbl_1 = new System.Windows.Forms.Label();
+            this.Btn_1 = new System.Windows.Forms.Button();
+            this.Btn_2 = new System.Windows.Forms.Button();
+            this.Btn_3 = new System.Windows.Forms.Button();
+            this.Btn_4 = new System.Windows.Forms.Button();
+            this.Btn_5 = new System.Windows.Forms.Button();
+            this.Btn_6 = new System.Windows.Forms.Button();
+            this.Btn_7 = new System.Windows.Forms.Button();
+            this.Btn_8 = new System.Windows.Forms.Button();
+            this.Btn_9 = new System.Windows.Forms.Button();
+            this.Btn_00 = new System.Windows.Forms.Button();
+            this.Btn_0 = new System.Windows.Forms.Button();
+            this.Btn_Kekka = new System.Windows.Forms.Button();
+            this.Btn_clear = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Txt_1
             // 
-            this.Txt_1.Location = new System.Drawing.Point(165, 95);
+            this.Txt_1.Location = new System.Drawing.Point(44, 56);
+            this.Txt_1.Multiline = true;
             this.Txt_1.Name = "Txt_1";
-            this.Txt_1.Size = new System.Drawing.Size(148, 19);
+            this.Txt_1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.Txt_1.Size = new System.Drawing.Size(220, 46);
             this.Txt_1.TabIndex = 0;
             // 
             // Lbl_1
             // 
             this.Lbl_1.AutoSize = true;
-            this.Lbl_1.Location = new System.Drawing.Point(203, 59);
+            this.Lbl_1.Location = new System.Drawing.Point(347, 46);
             this.Lbl_1.Name = "Lbl_1";
             this.Lbl_1.Size = new System.Drawing.Size(35, 12);
             this.Lbl_1.TabIndex = 1;
             this.Lbl_1.Text = "label1";
             // 
+            // Btn_1
+            // 
+            this.Btn_1.Location = new System.Drawing.Point(122, 359);
+            this.Btn_1.Name = "Btn_1";
+            this.Btn_1.Size = new System.Drawing.Size(64, 43);
+            this.Btn_1.TabIndex = 2;
+            this.Btn_1.Text = "1";
+            this.Btn_1.UseVisualStyleBackColor = true;
+            this.Btn_1.Click += new System.EventHandler(this.Btn_1_Click);
+            // 
+            // Btn_2
+            // 
+            this.Btn_2.Location = new System.Drawing.Point(234, 359);
+            this.Btn_2.Name = "Btn_2";
+            this.Btn_2.Size = new System.Drawing.Size(64, 43);
+            this.Btn_2.TabIndex = 3;
+            this.Btn_2.Text = "2";
+            this.Btn_2.UseVisualStyleBackColor = true;
+            this.Btn_2.Click += new System.EventHandler(this.Btn_2_Click_1);
+            // 
+            // Btn_3
+            // 
+            this.Btn_3.Location = new System.Drawing.Point(349, 359);
+            this.Btn_3.Name = "Btn_3";
+            this.Btn_3.Size = new System.Drawing.Size(64, 43);
+            this.Btn_3.TabIndex = 4;
+            this.Btn_3.Text = "3";
+            this.Btn_3.UseVisualStyleBackColor = true;
+            this.Btn_3.Click += new System.EventHandler(this.Btn_3_Click_1);
+            // 
+            // Btn_4
+            // 
+            this.Btn_4.Location = new System.Drawing.Point(122, 285);
+            this.Btn_4.Name = "Btn_4";
+            this.Btn_4.Size = new System.Drawing.Size(64, 43);
+            this.Btn_4.TabIndex = 5;
+            this.Btn_4.Text = "4";
+            this.Btn_4.UseVisualStyleBackColor = true;
+            this.Btn_4.Click += new System.EventHandler(this.Btn_4_Click_1);
+            // 
+            // Btn_5
+            // 
+            this.Btn_5.Location = new System.Drawing.Point(234, 285);
+            this.Btn_5.Name = "Btn_5";
+            this.Btn_5.Size = new System.Drawing.Size(64, 43);
+            this.Btn_5.TabIndex = 6;
+            this.Btn_5.Text = "5";
+            this.Btn_5.UseVisualStyleBackColor = true;
+            this.Btn_5.Click += new System.EventHandler(this.Btn_5_Click_1);
+            // 
+            // Btn_6
+            // 
+            this.Btn_6.Location = new System.Drawing.Point(349, 285);
+            this.Btn_6.Name = "Btn_6";
+            this.Btn_6.Size = new System.Drawing.Size(64, 43);
+            this.Btn_6.TabIndex = 7;
+            this.Btn_6.Text = "6";
+            this.Btn_6.UseVisualStyleBackColor = true;
+            this.Btn_6.Click += new System.EventHandler(this.Btn_6_Click_1);
+            // 
+            // Btn_7
+            // 
+            this.Btn_7.Location = new System.Drawing.Point(122, 207);
+            this.Btn_7.Name = "Btn_7";
+            this.Btn_7.Size = new System.Drawing.Size(64, 43);
+            this.Btn_7.TabIndex = 8;
+            this.Btn_7.Text = "7";
+            this.Btn_7.UseVisualStyleBackColor = true;
+            this.Btn_7.Click += new System.EventHandler(this.Btn_7_Click_1);
+            // 
+            // Btn_8
+            // 
+            this.Btn_8.Location = new System.Drawing.Point(234, 207);
+            this.Btn_8.Name = "Btn_8";
+            this.Btn_8.Size = new System.Drawing.Size(64, 43);
+            this.Btn_8.TabIndex = 9;
+            this.Btn_8.Text = "8";
+            this.Btn_8.UseVisualStyleBackColor = true;
+            this.Btn_8.Click += new System.EventHandler(this.Btn_8_Click_1);
+            // 
+            // Btn_9
+            // 
+            this.Btn_9.Location = new System.Drawing.Point(349, 207);
+            this.Btn_9.Name = "Btn_9";
+            this.Btn_9.Size = new System.Drawing.Size(64, 43);
+            this.Btn_9.TabIndex = 10;
+            this.Btn_9.Text = "9";
+            this.Btn_9.UseVisualStyleBackColor = true;
+            this.Btn_9.Click += new System.EventHandler(this.Btn_9_Click_1);
+            // 
+            // Btn_00
+            // 
+            this.Btn_00.Location = new System.Drawing.Point(234, 435);
+            this.Btn_00.Name = "Btn_00";
+            this.Btn_00.Size = new System.Drawing.Size(64, 43);
+            this.Btn_00.TabIndex = 12;
+            this.Btn_00.Text = "00";
+            this.Btn_00.UseVisualStyleBackColor = true;
+            this.Btn_00.Click += new System.EventHandler(this.Btn_00_Click_1);
+            // 
+            // Btn_0
+            // 
+            this.Btn_0.Location = new System.Drawing.Point(122, 435);
+            this.Btn_0.Name = "Btn_0";
+            this.Btn_0.Size = new System.Drawing.Size(64, 43);
+            this.Btn_0.TabIndex = 11;
+            this.Btn_0.Text = "0";
+            this.Btn_0.UseVisualStyleBackColor = true;
+            this.Btn_0.Click += new System.EventHandler(this.Btn_0_Click_1);
+            // 
+            // Btn_Kekka
+            // 
+            this.Btn_Kekka.Location = new System.Drawing.Point(464, 89);
+            this.Btn_Kekka.Name = "Btn_Kekka";
+            this.Btn_Kekka.Size = new System.Drawing.Size(64, 43);
+            this.Btn_Kekka.TabIndex = 13;
+            this.Btn_Kekka.Text = "計算";
+            this.Btn_Kekka.UseVisualStyleBackColor = true;
+            // 
+            // Btn_clear
+            // 
+            this.Btn_clear.Location = new System.Drawing.Point(464, 167);
+            this.Btn_clear.Name = "Btn_clear";
+            this.Btn_clear.Size = new System.Drawing.Size(64, 43);
+            this.Btn_clear.TabIndex = 14;
+            this.Btn_clear.Text = "clear";
+            this.Btn_clear.UseVisualStyleBackColor = true;
+            this.Btn_clear.Click += new System.EventHandler(this.Btn_clear_Click_1);
+            // 
             // F_Kaikei
             // 
-            this.ClientSize = new System.Drawing.Size(548, 438);
+            this.ClientSize = new System.Drawing.Size(628, 549);
+            this.Controls.Add(this.Btn_clear);
+            this.Controls.Add(this.Btn_Kekka);
+            this.Controls.Add(this.Btn_00);
+            this.Controls.Add(this.Btn_0);
+            this.Controls.Add(this.Btn_9);
+            this.Controls.Add(this.Btn_8);
+            this.Controls.Add(this.Btn_7);
+            this.Controls.Add(this.Btn_6);
+            this.Controls.Add(this.Btn_5);
+            this.Controls.Add(this.Btn_4);
+            this.Controls.Add(this.Btn_3);
+            this.Controls.Add(this.Btn_2);
+            this.Controls.Add(this.Btn_1);
             this.Controls.Add(this.Lbl_1);
             this.Controls.Add(this.Txt_1);
             this.Name = "F_Kaikei";
@@ -64,6 +221,19 @@
 
         private System.Windows.Forms.TextBox Txt_1;
         private System.Windows.Forms.Label Lbl_1;
+        private System.Windows.Forms.Button Btn_1;
+        private System.Windows.Forms.Button Btn_2;
+        private System.Windows.Forms.Button Btn_3;
+        private System.Windows.Forms.Button Btn_4;
+        private System.Windows.Forms.Button Btn_5;
+        private System.Windows.Forms.Button Btn_6;
+        private System.Windows.Forms.Button Btn_7;
+        private System.Windows.Forms.Button Btn_8;
+        private System.Windows.Forms.Button Btn_9;
+        private System.Windows.Forms.Button Btn_00;
+        private System.Windows.Forms.Button Btn_0;
+        private System.Windows.Forms.Button Btn_Kekka;
+        private System.Windows.Forms.Button Btn_clear;
     }
 }
 
--- akasaka_sakasu/F_Kaikei.cs
+++ akasaka_sakasu/F_Kaikei.cs
@@ -12,6 +12,12 @@ namespace akasaka_sakasu
 {
     public partial class F_Kaikei : Form
     {
+        //初期変数
+        //int Int1 = 1,Int2 = 2, Int3 = 3, Int4 = 4, Int5 = 5, Int6 = 6, Int7 = 7, Int8 = 8, Int9 = 9, Int0 = 0, Int00 = 00;
+        string StrKekka;
+        int IntCount;
+
+
         public F_Kaikei()
         {
             InitializeComponent();
@@ -19,10 +25,91 @@ namespace akasaka_sakasu
 
         private void F_Kaikei_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("hellooo");
-            MessageBox.Show("hellootest");
-            MessageBox.Show("hellootest210201テスト");
+            
         }
+
+        #region ボタン動作
+
+        private void Btn_1_Click(object sender, EventArgs e)
+        {
+            //テキストボックスに押す前の値+押したボタンの値を加える
+            //関数可したい
+            //Txt_1.Text = StrKekka + "1";
+           // StrKekka = Txt_1.Text;
+            Btnkeisan("1");
+        }
+
+        private void Btn_2_Click_1(object sender, EventArgs e)
+        {
+            Btnkeisan("2");
+        }
+
+        private void Btn_3_Click_1(object sender, EventArgs e)
+        {
+            Btnkeisan("3");
+        }
+
+        private void Btn_4_Click_1(object sender, EventArgs e)
+        {
+            Btnkeisan("4");
+        }
+
+        private void Btn_5_Click_1(object sender, EventArgs e)
+        {
+            Btnkeisan("5");
+        }
+
+        private void Btn_6_Click_1(object sender, EventArgs e)
+        {
+            Btnkeisan("6");
+        }
+
+        private void Btn_7_Click_1(object sender, EventArgs e)
+        {
+            Btnkeisan("7");
+        }
+
+        private void Btn_8_Click_1(object sender, EventArgs e)
+        {
+            Btnkeisan("8");
+        }
+
+        private void Btn_9_Click_1(object sender, EventArgs e)
+        {
+            Btnkeisan("9");
+        }
+
+        private void Btn_0_Click_1(object sender, EventArgs e)
+        {
+            Btnkeisan("0");
+        }
+
+        private void Btn_00_Click_1(object sender, EventArgs e)
+        {
+            Btnkeisan("00");
+        }
+
+        private void Btn_clear_Click_1(object sender, EventArgs e)
+        {
+            Txt_1.Clear();
+        }
+        #endregion
+
+        #region 関数
+
+        //関数
+        public void Btnkeisan(string strKeisan)
+        {
+            Txt_1.Text = StrKekka + strKeisan;
+            StrKekka = Txt_1.Text;
+
+        }
+
+        #endregion
+
     }
 }
+
+
     
+
